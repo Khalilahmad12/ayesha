e0f76a504941c52f335321fac5b19e40f8e84676
Khalil Ahmad <kheelomahi124@gmail.com>
2026-01-19 22:06:31 -0800

    Fix About module imports for case-sensitive build

--- src/modules/about/Index.jsx
+++ src/modules/about/Index.jsx
@@ -1,7 +1,7 @@
 import { Server } from "lucide-react";
 import HeroSection from "./sections/hero-section/HeroSection";
 import PropertyTypes from "./sections/property-types/PropertyTypes";
-import TeamSection from "./sections/Team/TeamSection";
+import TeamSection from "./sections/team/TeamSection";
 import TrustedAdvisors from "./sections/trusted-advisers/TrustedAdvisors";
 import ServicesSection from './sections/services/ServicesSection'
 import PropertySection from './sections/property-section/PropertySection'
